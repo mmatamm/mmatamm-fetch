9d1c4579d743a0a2a298f4f18196cd87faa93235
Amit Goren <animalfarm@mailfence.com>
2024-07-03 18:24:21 +0300

    feat: add a progress bar

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,19 @@ mod ingress;
 mod ohlc;
 mod process_dump;
 
-use std::error::Error;
+use std::{error::Error, time::Duration};
 
 use anyhow::Context;
 use chrono::NaiveDate;
 use clap::Parser;
 use concurrent_for_each::concurrent_for_each;
-use flexi_logger::Logger;
+use flexi_logger::{colored_detailed_format, json_format, Logger};
 use hist::DumpMetadata;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif_log_bridge::LogWrapper;
 use ingress::{ingress_regularly, pseudo_ingress};
 use isahc::AsyncReadResponseExt;
-use log::{error, info};
+use log::{error, info, logger};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -72,8 +74,10 @@ fn filter_dump(args: &Args, dump: &DumpMetadata) -> bool {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    // Start the logger
-    Logger::try_with_env_or_str("info")?.start()?;
+    // Start the logger and ensure it doesn't interfere with progress bars
+    let (boxed_logger, _logger_handle) = Logger::try_with_env_or_str("info")?.build()?;
+    let progress_bars = MultiProgress::new();
+    LogWrapper::new(progress_bars.clone(), boxed_logger).try_init()?;
 
     // Parse CLI argumetns
     let args = Args::parse();
@@ -111,20 +115,39 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // tokio::task::spawn_blocking(move || ingress_regularly(sender, ticks_receiver, 10000));
     tokio::task::spawn_blocking(move || pseudo_ingress(ticks_receiver));
 
+    // Create a sexy progress bar
+    let total_progress = progress_bars.add(
+        ProgressBar::new(relevant_dumps.len() as u64)
+            .with_prefix("Processing IEX dumps")
+            .with_style(
+                ProgressStyle::default_bar()
+                    .template("{prefix}: {wide_bar} {human_pos}/{human_len}, ETA: {eta}")
+                    .unwrap(),
+            ),
+    );
+    total_progress.enable_steady_tick(Duration::from_secs(10));
+
     // BUG This caused the compiler to ICE. It is fixed in version 1.8.1.
     concurrent_for_each(
         |dump| {
-            let value = ticks_sender.clone();
+            let local_ticks_sender = ticks_sender.clone();
+            let local_progress_bar = total_progress.clone();
+
             async move {
-                if let Err(err) = process_dump::read_dump(dump, value).await {
+                if let Err(err) = process_dump::read_dump(dump, local_ticks_sender).await {
                     error!(dump:?; "{:#}", err);
                 }
+
+                local_progress_bar.inc(1);
             }
         },
         relevant_dumps,
         args.workers,
     )
     .await?;
+
+    total_progress.finish();
+    progress_bars.remove(&total_progress);
 
     Ok(())
 }
